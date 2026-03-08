7a5302b82e6cce6504166e9fee6525922e8a23b7
AmberAlter <231804657@qq.com>
2026-03-08 14:36:14 +0800

    update

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -9,11 +9,16 @@
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
 // all, so there'll be no more left :(
 fn maybe_icecream(time_of_day: u16) -> Option<u16> {
-    // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a
-    // value of 0 The Option output should gracefully handle cases where
-    // time_of_day > 23.
-    // TODO: Complete the function body - remember to return an Option!
-    ???
+    if time_of_day > 23 {
+        // 时间非法，返回 None
+        None
+    } else if time_of_day >= 22 {
+        // 太晚了，没有冰激凌了 (返回 Some(0))
+        Some(0)
+    } else {
+        // 其他时间给 5 个
+        Some(5)
+    }
 }
 
 #[cfg(test)]
@@ -34,6 +39,6 @@ mod tests {
         // TODO: Fix this test. How do you get at the value contained in the
         // Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams.unwrap(), 5);
     }
 }
